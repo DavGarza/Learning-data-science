66ee0698f87354ae7e426b0b0fddfc2ae7ef1301
David Garza <dgza97@gmail.com>
2022-04-04 19:07:01 -0500

    Adding changes in mananing_errors

--- python_projects/intermidiate_python/managing_errors.py
+++ python_projects/intermidiate_python/managing_errors.py
@@ -1,10 +1,13 @@
 user_number = int(input("Write a whole number and discover if it is a prime number"))
 
-for i in range(2,user_number//2):
-    if(user_number % i) == 0:
-        print(user_number, "is not a prime number")
-        break      
-    else:
-        print(user_number, "is a prime number")
+def main():
 
+    for i in range(2, user_number // 2):
+        if(user_number % i) == 0:
+            print(user_number, "is not a prime number")
+            break      
+        else:
+            print(user_number, "is a prime number")
 
+if __name__ == "__main__":
+    main()
